b84d5bd9b5e78c7f0cc2649257335610a07d6565
Nathan Mcfadyen <94457526+GamingGuy4321@users.noreply.github.com>
2022-10-18 16:56:51 -0300

    Added Player, and First Map

---
+++ DungeonRunnerUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    Rigidbody2D m_rigidbody;
+    // Get access to the Animator on this object
+    [HideInInspector]
+    public Animator m_animator;
+    // A simple check to see if our player is giving any movement input
+    [HideInInspector]
+    public bool m_isMoving = false;
+    // Variables to access input from the player
+    float m_horizontalMovement;
+    float m_verticalMovement;
+    // Speed variable to adjust how fast the player moves on screen
+    public float m_moveSpeed = 5.0f;
+
+    void Start()
+    {
+        // On start, get the Rigidbody2D component and assign it to m_rigidbody
+        m_rigidbody = GetComponent<Rigidbody2D>();
+        // On start, get the Animator component and assign it to the m_animator
+        m_animator = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+    void FixedUpdate() {
+        // Every frame, check for input from the "Horizontal" and "Vertical" inputs and assign them to the values accordingly
+        m_verticalMovement = Input.GetAxisRaw("Vertical");
+        // Add velocity to the Rigidbody component using the input values, adding the relative move speed we've assigned earlier
+        m_rigidbody.velocity = new Vector2(0.0f, m_verticalMovement * m_moveSpeed);
+    }
+}
